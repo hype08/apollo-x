70d8920c94b18f3bc4ccb3b3e5b6f6ad158342a9
henry <tarou.88@gmail.com>
2019-11-16 22:26:00 +0900

    ✨mutation resolvers
    login + book

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,56 +2,74 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  # Queries for the current user
-  me: User
-}
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
 
-type Mutation {
-  # if false, booking trips failed -- check errors
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
 
-  # if false, cancellation failed -- check errors
-  cancelTrip(launchId: ID!): TripUpdateResponse!
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
 
-  login(email: String): String # login token
-}
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
 
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
-}
+    login(email: String): String # login token
+  }
 
-type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
-type Rocket {
-  id: ID!
-  name: String
-  type: String
-}
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
 
-type User {
-  id: ID!
-  email: String!
-  trips: [Launch]!
-}
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
-}
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-enum PatchSize {
-  SMALL
-  LARGE
-}
+  type Mission {
+    missionPatch(mission: String, size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 `;
 
 module.exports = typeDefs;
