e8638b413f092d91483d9938b796f9eff1d94cb5
henry <tarou.88@gmail.com>
2019-11-17 23:08:20 +0900

    ✨log-out, cart-items, book-trips

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "@apollo/react-hooks";
-import React from "react";
-import ReactDOM from "react-dom";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+
 import Pages from "./pages";
+import Login from './pages/login';
+import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
+
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: "http://localhost:4000/"
-});
-
 const client = new ApolloClient({
   cache,
-  link
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+  typeDefs,
+  resolvers,
 });
 
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }  
+`
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Pages />
